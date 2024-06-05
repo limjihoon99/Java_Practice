f46042a5fd46a2a42433ecfe7acc8a7472b8f183
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-06-05 17:26:14 +0900

    Update Application.java

--- 02_Operator/src/com/beyond/operator/Application.java
+++ 02_Operator/src/com/beyond/operator/Application.java
@@ -5,6 +5,8 @@ import com.beyond.operator.practice.B_InDecrease;
 import com.beyond.operator.practice.C_Arithmetic;
 import com.beyond.operator.practice.D_Comparison;
 import com.beyond.operator.practice.E_Logical;
+import com.beyond.operator.practice.F_Assignment;
+import com.beyond.operator.practice.G_Triple;
 
 public class Application {
 	public static void main(String[] args) {
@@ -13,6 +15,12 @@ public class Application {
 		// new B_InDecrease().method2();
 		// new C_Arithmetic().method1();
 		// new D_Comparison().method2();
-		new E_Logical().method1();
+		// new E_Logical().method2();
+		// new E_Logical().practice();
+		// new F_Assignment().method1();
+		// new G_Triple().method1();
+//		new G_Triple().practice1();
+//		new G_Triple().practice2();
+		new G_Triple().practice3();
 	}
 }
