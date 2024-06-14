9490c62f729f6e2a78d71b8e5b557d7f315475d3
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-06-14 17:26:48 +0900

    feat: create 17_List file

---
+++ 17_List/src/com/beyond/list/practice/Music.java
@@ -0,0 +1,31 @@
+package com.beyond.list.practice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Music implements Comparable<Music> {
+	private String title;
+	
+	private String artist;
+	
+	private int ranking;
+
+	/*
+	 * compareTo() 
+	 *   - 메소드에서 반환되는 값을 가지고 정렬 기준을 잡는다.
+	 *   - 비교해서 같으면 0을 리턴한다.
+	 *   - 비교해서 자신이 크다면 양의 정수를 리턴한다.
+	 *   - 비교해서 자신이 작다면 음의 정수를 리턴한다.
+	 */
+	@Override
+	public int compareTo(Music music) {
+		// 랭킹 순서대로 정렬하는 코드 (오름차순 정렬)
+		
+		return this.ranking - music.ranking;
+//		return music.ranking - this.ranking; // 내림차순 정렬
+	}
+}
