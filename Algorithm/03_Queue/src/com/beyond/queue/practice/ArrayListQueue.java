3aa47e72be5f17777694a5b6be4def51020e3432
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-06-21 13:31:41 +0900

    feat: craete 03_Queue file

---
+++ Algorithm/03_Queue/src/com/beyond/queue/practice/ArrayListQueue.java
@@ -0,0 +1,41 @@
+package com.beyond.queue.practice;
+
+public class ArrayListQueue<T> implements Queue<T> {
+
+	@Override
+	public void enqueue(T value) {
+		
+		
+	}
+
+	@Override
+	public int size() {
+		
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		
+		return false;
+	}
+
+	@Override
+	public T dequeue() {
+		
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		
+		return null;
+	}
+
+	@Override
+	public boolean contains(T value) {
+		
+		return false;
+	}
+
+}
